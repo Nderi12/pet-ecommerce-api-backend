c84fa9f5116c190ed7f7ae5b14a353b4966331aa
Nderi12 <nderikamau1212@gmail.com>
2023-03-17 21:50:34 +0300

    Adding migrations to store data foreach model

---
+++ database/migrations/2023_03_17_183420_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid')->unique();
+            $table->foreignIdFor(Brand::class)->constrained();
+            $table->string('category_uuid');
+            $table->string('title');
+            $table->float('price');
+            $table->text('description');
+            $table->json('metadata')->nullable();
+            $table->timestamp('deleted_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
