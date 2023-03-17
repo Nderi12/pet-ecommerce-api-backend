ac73843a25509755f3694c8fb9fff5355440241f
Nderi12 <nderikamau1212@gmail.com>
2023-03-17 22:00:02 +0300

    Adding protected fillables in each model

---
+++ app/Models/Product.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory, HasUuid;
+
+    /**
+     * Protected fillables
+     *
+     * @var array
+     * @author Nderi Kamau <nderikamau1212@gmail.com>
+     */
+    protected $fillable = [
+        'title',
+        'price',
+        'description',
+        'metadata',
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
+    protected $casts = [
+        'metadata' => 'json',
+    ];
+}
