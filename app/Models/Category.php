cf6af076e670ad3df8ec57cc281c1c34c2d6c2ed
Nderi12 <nderikamau1212@gmail.com>
2023-03-19 09:34:21 +0300

    Adding boot method for category slugs

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Traits\HasUuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
@@ -20,4 +21,32 @@ class Category extends Model
         'slug',
         'title',
     ];
+
+    /**
+     * Static method called automatically when the model is booted
+     *
+     * @var array
+     * @author Nderi Kamau <nderikamau1212@gmail.com>
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        // Closure that is executed when a new instance of the model is being created.
+        static::creating(function ($category) {
+            // Set the value of the slug on the model generated from the name
+            $category->slug = Str::slug($category->name);
+        });
+    }
+
+    /**
+     * Get all products to a category
+     *
+     * @return void
+     * @author Nderi Kamau <nderikamau1212@gmail.com>
+     */
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
