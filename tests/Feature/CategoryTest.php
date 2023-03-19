9d971fc14b692f50b76fd6ade7a4adde71a4da2d
Nderi12 <nderikamau1212@gmail.com>
2023-03-19 19:27:20 +0300

    Adding category feature test

---
+++ tests/Feature/CategoryTest.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Models\Category;
+
+class CategoryTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Test for getting all categories via API.
+     *
+     * @return void
+     */
+    public function testGetAllCategories()
+    {
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $this->getJwtToken(),
+        ])->get('/api/v1/categories');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test for creating a category via API.
+     *
+     * @return void
+     */
+    public function testCreateCategory()
+    {
+        $category = [
+            'slug' => 'test-category',
+            'title' => 'Test Category',
+        ];
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $this->getJwtToken(),
+        ])->post('/api/v1/category/create', $category);
+
+        $response->assertStatus(201);
+        $this->assertDatabaseHas('categories', $category);
+    }
+
+    /**
+     * Test for updating a category via API.
+     *
+     * @return void
+     */
+    public function testUpdateCategory()
+    {
+        $category = Category::factory()->create();
+
+        $updatedCategory = [
+            'slug' => 'updated-category',
+            'title' => 'Updated Category',
+        ];
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $this->getJwtToken(),
+        ])->put('/api/v1/category/' . $category->uuid, $updatedCategory);
+
+        $response->assertStatus(200);
+        $this->assertDatabaseHas('categories', $updatedCategory);
+        $this->assertDatabaseMissing('categories', $category->toArray());
+    }
+
+    /**
+     * Test for deleting a category via API.
+     *
+     * @return void
+     */
+    public function testDeleteCategory()
+    {
+        $category = Category::factory()->create();
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $this->getJwtToken(),
+        ])->delete('/api/v1/category/' . $category->uuid);
+
+        $response->assertStatus(200);
+        $this->assertDatabaseMissing('categories', $category->toArray());
+    }
+
+    /**
+     * Test for getting a single category via API.
+     *
+     * @return void
+     */
+    public function testGetSingleCategory()
+    {
+        $category = Category::factory()->create();
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $this->getJwtToken(),
+        ])->get('/api/v1/category/' . $category->uuid);
+
+        $category = [
+            'slug' => $category->slug,
+            'title' => $category->title,
+        ];
+
+        $response->assertStatus(200);
+        $this->assertDatabaseHas('categories', $category);
+    }
+
+    /**
+     * Helper function to get JWT token.
+     *
+     * @return string
+     */
+    private function getJwtToken()
+    {
+        // Implement your logic to get a valid JWT token for authentication
+        // and return it as a string.
+        $token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImZvbyI6ImJhciJ9.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0IiwiYXVkIjoiaHR0cDovL2xvY2FsaG9zdCIsImlhdCI6MTY3OTI0MDk5MC4yNTQ4NzcsIm5iZiI6MTY3OTI0MTA1MC4yNTQ4NzcsImV4cCI6MTY3OTI0NDU5MC4yNTQ4NzcsInVpZCI6MX0.lBXJ0q5qfRvdjMM6tZNEwaikqTl6AQiM8mCojLEbM_I";
+        return $token;
+    }
+}
