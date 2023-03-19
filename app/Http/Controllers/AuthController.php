9acccc6e83c656ffd30c030840cf56ec6870be0a
Nderi12 <nderikamau1212@gmail.com>
2023-03-19 16:23:17 +0300

    Adding login, register functions

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,18 +2,89 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use DateTimeImmutable;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
-use Lcobucci\JWT\JwtFacade;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 use Lcobucci\JWT\Signer\Key\InMemory;
 use Lcobucci\JWT\Signer\Rsa\Sha256;
+use Lcobucci\JWT\Builder;
+use Lcobucci\JWT\JwtFacade;
 
 class AuthController extends Controller
 {
-    public function login()
+    public function __construct()
     {
+        $this->middleware('guest')->except('logout');
+    }
 
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'address' => $request->address,
+            'phone_number' => $request->phone_number,
+            'password' => bcrypt($request->password),
+        ]);
+
+        return response()->json([
+            'message' => 'Successfully registered.',
+            'user' => $user
+        ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+
+            $token = $this->issueToken($user->id);
+
+            return response()->json([
+                'message' => 'Successfully logged in.',
+                'token' => $token
+            ], 200);
+        }
+
+        return response()->json([
+            'message' => 'Invalid email or password.'
+        ], 401);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->json([
+            'message' => 'Successfully logged out.'
+        ], 200);
     }
 
     public function issueToken($user_id)
@@ -35,6 +106,6 @@ class AuthController extends Controller
                 ->expiresAt($issuedAt->modify('+60 minutes'))
         );
 
-        return $token->toString();        
+        return $token->toString();
     }
 }
