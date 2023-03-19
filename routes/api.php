91723c4df6c577cacd7d9fdbbe35aa353796585b
Nderi12 <nderikamau1212@gmail.com>
2023-03-19 18:28:06 +0300

    Updating admin apis prefix

--- routes/api.php
+++ routes/api.php
@@ -18,11 +18,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::group(['prefix' => 'v1'], function() {
+Route::group(['prefix' => 'v1/admin'], function() {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
 });
