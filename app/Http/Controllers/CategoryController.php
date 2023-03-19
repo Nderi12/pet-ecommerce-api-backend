22400ed0749f92cdc1e72eecac862458fd8ac1e7
Nderi12 <nderikamau1212@gmail.com>
2023-03-19 12:12:38 +0300

    Adding swagger open api commnets for docs

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -16,15 +16,29 @@ class CategoryController extends Controller
      * @return void
      * @author Nderi Kamau <nderikamau1212@gmail.com>
      */
-    public function __construct() {
+    public function __construct()
+    {
         // Functions inside the authentication controller can not be accessed without having the valid token.
-        $this->middleware('auth:api');
+        // $this->middleware('auth:api');
     }
-    
+
     /**
-     * Display a listing of the resource.
-     * 
-     * @author Nderi Kamau <nderikamau1212@gmail.com>
+     * @OA\Get(
+     *     path="/categories",
+     *     summary="Get all categories",
+     *     tags={"Categories"},
+     *     @OA\Response(
+     *         response="200",
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="categories",
+     *                 type="array",
+     *                 @OA\Items(ref="#/components/schemas/Category")
+     *             )
+     *         )
+     *     )
+     * )
      */
     public function index()
     {
@@ -37,9 +51,27 @@ class CategoryController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
-     * 
-     * @author Nderi Kamau <nderikamau1212@gmail.com>
+     * @OA\Post(
+     *     path="/categories",
+     *     summary="Create a new category",
+     *     tags={"Categories"},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         description="Category object that needs to be created",
+     *         @OA\JsonContent(ref="#/components/schemas/CategoryRequest")
+     *     ),
+     *     @OA\Response(
+     *         response="201",
+     *         description="Category created successfully",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 description="A message indicating that the category was created successfully."
+     *             )
+     *         )
+     *     )
+     * )
      */
     public function store(CategoryRequest $request)
     {
@@ -49,37 +81,90 @@ class CategoryController extends Controller
         // Create an empty category
         $category = Category::make();
 
-        DB::transaction(function () use($data, $category) {
-           // create a new category from the validated data
-           $category->create($data);
+        DB::transaction(function () use ($data, $category) {
+            // create a new category from the validated data
+            $category->create($data);
         });
 
         // Return response with message and data
         return response()->json([
-            'message' => 'Category created successfully!',
-            'category' => $category
+            'message' => 'Category created successfully!'
         ], Response::HTTP_CREATED); // 201 response for created
     }
 
     /**
-     * Display the specified resource.
-     * 
-     * @author Nderi Kamau <nderikamau1212@gmail.com>
+     * @OA\Get(
+     *     path="/categories/{uuid}",
+     *     summary="Get a single category by UUID",
+     *     tags={"Categories"},
+     *     @OA\Parameter(
+     *         name="uuid",
+     *         in="path",
+     *         description="UUID of the category to get",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string",
+     *             format="uuid"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="category",
+     *                 ref="#/components/schemas/Category"
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="404",
+     *         description="Category not found"
+     *     )
+     * )
      */
-    public function show(Category $category)
+    public function show($uuid)
     {
+        $category = Category::where('uuid', $uuid)->first();
+
         return response()->json([
             'category' => $category
         ], Response::HTTP_OK);
     }
 
     /**
-     * Update the specified resource in storage.
-     * 
-     * @author Nderi Kamau <nderikamau1212@gmail.com>
+     * @OA\Put(
+     *     path="/categories/{uuid}",
+     *     summary="Update a category",
+     *     tags={"Categories"},
+     *     @OA\Parameter(
+     *         name="uuid",
+     *         in="path",
+     *         description="The UUID of the category to update",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string",
+     *             format="uuid"
+     *         )
+     *     ),
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(ref="#/components/schemas/CategoryRequest")
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="Category updated successfully"
+     *     ),
+     *     @OA\Response(
+     *         response="404",
+     *         description="Category not found"
+     *     )
+     * )
      */
-    public function update(CategoryRequest $request, Category $category)
+    public function update(CategoryRequest $request, $uuid)
     {
+        $category = Category::where('uuid', $uuid)->first();
+
         //validate data
         $data = $request->validated();
 
@@ -93,17 +178,39 @@ class CategoryController extends Controller
     }
 
     /**
-     * Remove the specified resource from storage.
-     * 
-     * @author Nderi Kamau <nderikamau1212@gmail.com>
+     * @OA\Delete(
+     *     path="/categories/{uuid}",
+     *     summary="Delete a category",
+     *     tags={"Categories"},
+     *     @OA\Parameter(
+     *         name="uuid",
+     *         in="path",
+     *         description="The UUID of the category to delete",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string",
+     *             format="uuid"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="Category deleted successfully"
+     *     ),
+     *     @OA\Response(
+     *         response="404",
+     *         description="Category not found"
+     *     )
+     * )
      */
-    public function destroy(Category $category)
+    public function destroy($uuid)
     {
+        $category = Category::where('uuid', $uuid)->first();
+
         //  Delete the category
         $category->delete();
 
         // Return response with message
-        return redirect()->back()->with([
+        return response()->json([
             'success' => 'Category deleted successfully!'
         ], Response::HTTP_OK);
     }
