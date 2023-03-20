0fffbccbc6262a938a572de05e6d6658eb43c85c
Nderi12 <nderikamau1212@gmail.com>
2023-03-20 11:43:58 +0300

    adding user seeder to help with testing authentication

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::transaction(function () {
+            $user = User::create([
+                'name' => 'Admin user',
+                'email' => 'tspkeymanager1@gmail.com',
+                'phone_number' => '254722890101',
+                'address' => 'This is a sample address',
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
+            ]);
+        });
+    }
+}
