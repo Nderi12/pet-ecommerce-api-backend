25af3fd87bb9cc3fd2eb3b86225498fcb72a2de3
Nderi12 <nderikamau1212@gmail.com>
2023-03-20 14:45:15 +0300

    Adding blog request to validate data

---
+++ app/Models/Blog.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\HasUuid;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Blog extends Model
+{
+    use HasFactory, HasUuid;
+
+    protected $fillable = [
+        'slug',
+        'title',
+        'description',
+    ];
+}
