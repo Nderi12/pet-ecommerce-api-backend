c6ea78796b125186765f34217d8481ffd903217f
Nderi12 <nderikamau1212@gmail.com>
2023-03-20 14:49:04 +0300

    Minor change

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\OrderStatus;
+use BlogSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -22,5 +23,6 @@ class DatabaseSeeder extends Seeder
          $this->call(ProductSeeder::class);
          $this->call(OrderStatusSeeder::class);
          $this->call(UserSeeder::class);
+         $this->call(BlogSeeder::class);
     }
 }
